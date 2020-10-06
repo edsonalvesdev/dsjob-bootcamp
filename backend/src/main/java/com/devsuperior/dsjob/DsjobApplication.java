a31978ae54d9aabbd0204cdd91961c953ff42afd
Edson Alves <edson.f.alves09@gmail.com>
2020-10-05 23:18:03 -0300

    Final chapter work

---
+++ backend/src/main/java/com/devsuperior/dsjob/DsjobApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsjobApplication.class, args);
+	}
+
+}
